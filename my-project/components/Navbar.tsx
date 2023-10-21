9e07708bb5863bed58eb71b82e2ac805f7516b3a
lloydeebill <lloydbill.lariosa@gmail.com>
2023-10-21 15:38:12 +0800

    first commit

--- my-project/components/Navbar.tsx
+++ my-project/components/Navbar.tsx
@@ -5,6 +5,7 @@ import CustomButton from './CustomButton';
 
 
 const Navbar = () => {
+    const handleClick = "app/page.tsx"
   return (
     <header className='w-full absolute z-10'>
         <nav className='max-w-[1440px] mx-auto flex justify-between items-center 
